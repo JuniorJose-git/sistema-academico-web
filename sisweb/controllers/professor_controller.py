949e266c15d57fa4adffa2a38aa296d72989e703
José <jose>
2025-03-26 18:46:42 -0300

    função simples que mostra o nome do professor ao entrar na plataforma

--- sisweb/controllers/professor_controller.py
+++ sisweb/controllers/professor_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 from ..extensions import db
 from ..models.professor import Professor
@@ -9,6 +9,8 @@ professor = Blueprint('professor', __name__)
 
 @professor.route("/professor")
 def professor_json():
-    resp = db.session.execute(db.select(Professor)).scalars().all()
+    resp = db.session.execute(db.select(Professor).filter_by(id=10)).scalars().first()
 
-    return jsonify(resp)
+    # resp = db.session.execute(text("select * from professor where id=10")).first()
+
+    return render_template('index.html',professor=resp)
